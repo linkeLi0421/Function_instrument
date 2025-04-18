075e782ab230f6139e6275e45a7862323725e2b8
Linke <lilinke99@gmail.com>
2025-04-18 04:01:11 +0000

    update __print_func_name to dump more functions. (https://stackoverflow.com/questions/46728680/how-to-temporarily-suppress-output-from-printf?utm_source=chatgpt.com)

--- print_func.c
+++ print_func.c
@@ -2,5 +2,6 @@
 
 // Function to print the current function's name
 void __print_func_name(const char *name) {
-    printf("Entering function: %s\n", name);
+    fprintf(stderr, "Entering function: %s\n", name);
+    fflush(stderr);
 }
