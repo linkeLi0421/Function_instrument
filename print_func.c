1c00c2b9a01b65c5b707af0fcf1bb246c5d03ac3
Linke <lilinke99@gmail.com>
2025-04-25 16:59:44 +0000

    change __print_func to weak symbol, to avoid multiple defination error

--- print_func.c
+++ print_func.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 // Function to print the current function's name
-void __print_func_name(const char *name) {
+void __attribute__((weak)) __print_func_name(const char *name) {
     fprintf(stderr, "Entering function: %s\n", name);
     fflush(stderr);
 }
