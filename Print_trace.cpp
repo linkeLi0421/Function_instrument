6a26f9d01d51d05c407dbfec85735674669234e4
Linke <lilinke99@gmail.com>
2025-05-09 19:21:51 +0000

    print demangled function signature for cpp program

--- Print_trace.cpp
+++ Print_trace.cpp
@@ -1,14 +1,36 @@
-#include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/Module.h"
+#include "llvm/Demangle/Demangle.h"
+#include "llvm/Support/raw_ostream.h"
 
 
 using namespace llvm;
 
 namespace {
 struct Print_trace : PassInfoMixin<Print_trace> {
+
+  static StringRef buildPrettySignature(Function &F) {
+    // Create the string as a static local variable to ensure its lifetime
+    static std::string PrettySignature;
+    PrettySignature.clear();
+    
+    std::string DemName = demangle(F.getName().str());
+    FunctionType *FT = F.getFunctionType();
+    raw_string_ostream OS(PrettySignature);
+    FT->getReturnType()->print(OS);
+    OS << " " << DemName << "(";
+    for (unsigned i = 0, e = FT->getNumParams(); i != e; ++i) {
+      FT->getParamType(i)->print(OS);
+      if (i + 1 != e) OS << ", ";
+    }
+    OS << ")";
+    OS.flush();
+    return PrettySignature;
+  }
+
+
   PreservedAnalyses run(Function &F, FunctionAnalysisManager &) {
     // Skip function declarations (external functions)
     if (F.isDeclaration())
@@ -39,14 +61,33 @@ struct Print_trace : PassInfoMixin<Print_trace> {
       Builder.SetInsertPoint(&*Entry.begin());
     }
 
+    // Check if the file being compiled is C++ or C
+    StringRef FilePath = F.getParent()->getSourceFileName();
+    bool isCppFile = false;
+    if (FilePath.ends_with(".cpp") || FilePath.ends_with(".cc") || 
+      FilePath.ends_with(".cxx") || FilePath.ends_with(".C")) {
+      isCppFile = true;
+    } else if (FilePath.ends_with(".c")) {
+      isCppFile = false;
+    } else {
+      // If we can't determine by extension, check for C++ name mangling
+      isCppFile = F.getName().starts_with("_Z");
+    }
+
     // Create a global string for the function's own name.
     Value *funcName = Builder.CreateGlobalStringPtr(F.getName());
     // Insert a call to print_func_name with the function name.
-    Builder.CreateCall(printFunc, {funcName});
+    // Call print_func_name with the function name and demangle=0 (for raw function name)
+    Builder.CreateCall(printFunc, {funcName, Builder.getInt32(0)});
+
+    if (isCppFile) {
+      Value *funcName = Builder.CreateGlobalStringPtr(buildPrettySignature(F));
+      Builder.CreateCall(printFunc, {funcName, Builder.getInt32(1)});
+    }
   
       // Since we modified the IR, we do not preserve any analyses.
       return PreservedAnalyses::none();
-    }
+  }
   
     // Ensure the pass runs even for functions marked with optnone.
     static bool isRequired() { return true; }
--- print_func.c
+++ print_func.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 // Function to print the current function's name
-void __attribute__((weak)) __print_func_name(const char *name) {
-    fprintf(stderr, "Entering function: %s\n", name);
+void __attribute__((weak)) __print_func_name(const char *name, int demangle) {
+    if (demangle)
+        fprintf(stderr, "Demangled: %s\n", name);
+    else
+        fprintf(stderr, "Entering function: %s\n", name);
     fflush(stderr);
 }
